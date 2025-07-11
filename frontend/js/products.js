f7d0fc1bbd08cb5cec11b44075a411f5c4bb8dd6
Sumanth Naik <sumanthnaik02@gmail.com>
2025-07-11 15:34:41 +0530

    added front end

---
+++ frontend/js/products.js
@@ -0,0 +1,47 @@
+[
+  {
+    "id": 1,
+    "name": "Blue Chair",
+    "price": "₹8,817.00",
+    "image": "images/bluechair.png"
+  },
+  {
+    "id": 2,
+    "name": "Sun Glasses",
+    "price": "₹3,415.00",
+    "image": "images/sunglasses.png"
+  },
+  {
+    "id": 3,
+    "name": "Xiaomi Powerbank",
+    "price": "₹2,199",
+    "image": "images/powerbank.png"
+  },
+  {
+    "id": 4,
+    "name": "SEA-DWELLER",
+    "price": "₹3,649",
+    "image": "images/rolexwatch.png"
+  },
+  {
+    "id": 5,
+    "name": "Mens's Blazer",
+    "price": "₹2,724",
+    "image": "images/blazer.png"
+  },
+  {
+    "id": 6,
+    "name": "Macbook Pro MLH42",
+    "price": "₹3,28,590",
+    "image": "images/macbook.png"
+  },
+  {
+    "id": 7,
+    "name": "Nike Men Running shoe",
+    "price": "₹3,264",
+    "image": "images/shoes.png"
+  }
+]
+
+// fetch and display
+fetch("pro")
